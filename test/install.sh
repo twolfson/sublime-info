7c818de8fe5fcb58527a4c89e24fff00d89a23f2
Todd Wolfson <todd@twolfson.com>
2013-10-15 01:13:15 -0700

    Getting version at proper step

--- test/install.sh
+++ test/install.sh
@@ -2,7 +2,11 @@
 
 # If there is a Sublime Text version, then install Sublime
 if test -n "$SUBLIME_TEXT_VERSION"; then
+  # Use script for ease of use
   curl http://rawgithub.com/twolfson/sublime-installer/0.1.1/install.sh | sh -s $SUBLIME_TEXT_VERSION
+
+  # Output Sublime version
+  subl --version
 
   # If there is a rename, rename subl
   if test -n "$SUBLIME_TEXT_RENAME"; then
