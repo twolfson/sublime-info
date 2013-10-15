e118320b6c338014888b1cf7b9feec59c0d3365b
Todd Wolfson <todd@twolfson.com>
2013-10-15 00:47:41 -0700

    Continuing write up of code

--- sublime_info/sublime_info.py
+++ sublime_info/sublime_info.py
@@ -1,19 +1,45 @@
 # Load in dependencies
-import shutilwhich
-import shutil
+from shutilwhich import which
 
 
-# Define
+# Define a custom exception for when Sublime cannot be located
+class SublimeTextNotFoundException(Exception):
+    pass
+
+
+class SublimeTextNotResolvedException(SublimeTextNotFoundException):
+    pass
+
+
+class SublimeTextNotAtLocationException(SublimeTextNotFoundException):
+    pass
+
+
+# Define internal lookup which ignores
 def _get_sublime_path():
-    print shutil.which('subl')
+    # Attempt to resolve Sublime Text
+    path = (which('subl') or
+            which('sublime_text'))
+
+    # If Sublime is not found, raise our exception
+    if not path:
+        raise SublimeTextNotResolvedException(
+                'Sublime Text could not be found via the command "%s" or "%s"' %
+                ('subl',
+                 'sublime_text'))
+
+    # Otherwise, return the path
+    return path
 
 
 def get_sublime_path():
     """Resolve Sublime Text path
 
+    :raises SublimeTextNotFoundException: If Sublime Text cannot be found, an error will be raised
     :returns: If ``SUBLIME_TEXT_PATH`` is in OS environment, this will be returned.
               Otherwise, a ``which``-like resolution will be returned.
     :rtype: str
     """
     # TODO: environ
+    # TODO: If environ exists, still use os.path.exists
     return _get_sublime_path()
--- test/sublime-info_test.py
+++ test/sublime-info_test.py
@@ -16,8 +16,16 @@ Sublime Text as sublime_texttt
         resolved via `get_sublime_path`
             has a path of `/usr/bin/sublime_texttt`
 
+sublime_info
+    attempting to resolve Sublime Text
+        when it does not exist
+            raises a SublimeTextNotFoundException
+
 TODO: What about sublime_text.exe
 TODO: What about sublime_text.app
+
+TODO: Package dir
+TODO: Build version
 """
 
 
