a75a952c5a25182f6930c42946ffcb9aa2c57e5e
Todd Wolfson <todd@twolfson.com>
2013-10-15 00:31:23 -0700

    Documenting methods

--- sublime_info/sublime_info.py
+++ sublime_info/sublime_info.py
@@ -1,2 +1,12 @@
-def run():
+def _get_sublime_path():
+    pass
+
+
+def get_sublime_path():
+    """Resolve Sublime Text path
+
+    :returns: If ``SUBLIME_TEXT_PATH`` is in OS environment, this will be returned.
+              Otherwise, a ``which``-like resolution will be returned.
+    :rtype: str
+    """
     pass
