44382f158f3ff7e68916bc4b4a45fd8eea2e1d20
Todd Wolfson <todd@twolfson.com>
2013-12-24 19:20:47 -0800

    Removing null test

--- test_linux.sh
+++ test_linux.sh
@@ -1,2 +1,2 @@
 EXPECTED_PATH=/usr/bin/sublime_text EXPECTED_VERSION=2221 EXPECTED_PKG_DIR=/home/todd/.config/sublime-text-2/Packages nosetests --nocapture
-SUBLIME_TEXT_PATH=null EXPECT_ERROR=1 nosetests --nocapture
+# SUBLIME_TEXT_PATH=null EXPECT_ERROR=1 nosetests --nocapture
