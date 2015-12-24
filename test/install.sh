32b0fab6568ad827356af44eb3c6fa92e36d844f
Todd Wolfson <todd@twolfson.com>
2015-12-24 01:36:23 -0600

    Repaired URL for installer

--- test/install.sh
+++ test/install.sh
@@ -5,7 +5,7 @@ set -e
 # If there is a Sublime Text version, then install Sublime
 if test -n "$SUBLIME_TEXT_VERSION"; then
   # Use script for ease of use
-  curl http://rawgithub.com/twolfson/sublime-installer/0.2.0/install.sh | sh -s "$SUBLIME_TEXT_VERSION"
+  curl https://rawgit.com/twolfson/sublime-installer/0.2.0/install.sh | sh -s "$SUBLIME_TEXT_VERSION"
 
   # Output Sublime version
   subl --version
